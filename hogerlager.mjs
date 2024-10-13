59e6adcfb30d6358d1736998f87785e050f6dd2f
IgRiZ1 <dalil.blh@outlook.be>
2024-10-13 20:53:52 +0200

    Update hogerlager.mjs

--- hogerlager.mjs
+++ hogerlager.mjs
@@ -1,14 +1,13 @@
 function random(min, max , raden) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    
     if (raden == num){
         console.log('juist getal geraden'); 
          
     } else {
         console.log('fout getal');
     }
+}
 return num ;
   
-  }
   
   console.log(random(1,10,4));
